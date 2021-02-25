432cf993fa279e4b9ee99d56fdfa80d195e93fd4
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2021-02-25 12:09:26 +0000

    Added extra data file

---
+++ extra.asm
@@ -0,0 +1,9 @@
+; Extra data
+
+.HUMAN_ANIM
+  EQUB &10, &11, &12, &11
+.BOMBER_ANIM
+  EQUB &00, &01, &02, &01 ; Walk
+  EQUB &03, &04, &05, &04 ; Climb facing
+  EQUB &06, &07, &08, &07 ; Climb away
+  EQUB &09, &0A, &0B, &0C, &0D, &0E, &0F ; Explode
