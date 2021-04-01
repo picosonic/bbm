f0696290a84874ccaa63950c9451f85b6bae73e0
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2021-04-01 12:39:03 +0100

    Move some init from BASIC to downloader

--- downloader.asm
+++ downloader.asm
@@ -14,6 +14,29 @@ ORG downaddr
   ; Make sure we are not in decimal mode
   CLD
 
+  ; Set displayed chars per horizontal line (64) to match NES (256 pixels)
+  LDA #&01:STA CRTC00
+  LDA #&40:STA CRTC01
+
+  ; Shift display horizontally to centre (in char widths from left)
+  LDA #&02:STA CRTC00
+  LDA #&59:STA CRTC01
+
+  ; Set displayed chars per column (28) to match NES (240 pixels, with 224 displayed)
+  LDA #&06:STA CRTC00
+  LDA #&1C:STA CRTC01
+
+  ; Shift display vertically to centre
+  LDA #&07:STA CRTC00
+  LDA #&21:STA CRTC01
+
+  ; Change screen start to &4800 to gain an extra 6k bytes
+  LDA #&0D:STA CRTC00
+  LDA #(MODE8BASE) MOD 256:STA CRTC01
+  LDA #&0C:STA CRTC00
+  LDA #(MODE8BASE) DIV 256:LSR A:LSR A:LSR A:STA CRTC01
+
+  ; Initialisation
   LDA #&8C:LDX #&0C:JSR OSBYTE ; Select TAPE filing system with 1200 baud (to turn off DFS)
   LDA #&0F:LDX #&00:JSR OSBYTE ; Flush all buffers
   LDA #&C9:LDX #&01:LDY #&00:JSR OSBYTE ; Kbd irqs off!
@@ -28,7 +51,7 @@ ORG downaddr
   STA &2A1,X:DEX:BPL zaproms
  
   ; Clear zero page
-  LDX #&9F
+  LDX #&8F
 .clearzp
   STA 0,X:DEX:BNE clearzp
   STA 0
@@ -54,7 +77,31 @@ ORG downaddr
   INC relocate+5
   DEY:BNE relocate
 
+  ; Define ENVELOPE
+  LDA #&08
+  LDX #envelope MOD 256
+  LDY #envelope DIV 256
+  JSR OSWORD
+
+  ; Start game running
   JMP MAIN_RELOC_ADDR+&2200 ; Main entry point following relocation
+
+.envelope
+  ; "Marimba"
+  EQUB 1   ; Envelope number
+  EQUB 2   ; Length of each step (hundredths of a second) and auto repeat (top bit)
+  EQUB 0   ; Change of pitch per step in section 1
+  EQUB 0   ; Change of pitch per step in section 2
+  EQUB 0   ; Change of pitch per step in section 3
+  EQUB 0   ; Number of steps in section 1
+  EQUB 0   ; Number of steps in section 2
+  EQUB 0   ; Number of steps in section 3
+  EQUB 60 ; Change of amplitude per step during attack phase
+  EQUB -4  ; Change of amplitude per step during decay phase
+  EQUB -4  ; Change of amplitude per step during sustain phase
+  EQUB -4  ; Change of amplitude per step during release phase
+  EQUB 60 ; Target level at end of attack phase
+  EQUB 30  ; Target level at end of decay phase
 
 PRINT "Saving downloader from ", ~downaddr, " to ", ~P%
 SAVE "DOWNLOADER", downaddr, P%
--- loader.bas
+++ loader.bas
@@ -11,20 +11,12 @@ VDU23,1,0;0;0;0;:REM Hide cursor
 *FX15
 REPEATUNTILGET<>&FF
 :
-REM Initialise "MODE 8" (256x224)
 MODE1
-VDU19,1,0;0;19,2,0;0;19,3,0;0;19,4,0;0;
-?&FE00=1:?&FE01=64:REM Set 256 pixels wide
-?&FE00=2:?&FE01=89:REM Centre horizontally
-?&FE00=6:?&FE01=28:REM Set 224 pixels high
-?&FE00=7:?&FE01=33:REM Centre vertically
-?&FE00=13:?&FE01=0:?&FE00=12:?&FE01=9:REM Change screen start address to &4800
+VDU23,1,0;0;0;0;:REM Hide cursor
+VDU19,1,0;0;19,2,0;0;19,3,0;0;19,4,0;0;:REM Blank palette
 :
 REM Load extra datafile
 *L.EXTRA
-:
-REM Define envelope
-ENVELOPE 1,2,0,0,0,0,0,0,60,-4,-4,-4,60,30:REM "Marimba"
 :
 REM Load and run main game file
 */BBM
