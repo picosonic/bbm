ba60fae9655a8af41f1f02c2e544539a4e691646
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2023-01-11 15:11:02 +0000

    Added new data file

---
+++ swrdata.asm
@@ -0,0 +1,80 @@
+; Data to be loaded to sideways RAM
+
+.swrtitles
+INCBIN "TITLE.beeb"
+
+.swrtilesheet
+INCBIN "TILES.beeb"
+
+.swrspritesheet
+INCBIN "SPRITES.beeb"
+
+; Title
+.melody_01_c1
+INCBIN "melodies/M01C1.bin"
+.melody_01_c2
+INCBIN "melodies/M01C2.bin"
+.melody_01_c3
+INCBIN "melodies/M01C3.bin"
+
+; Stage screen
+.melody_02_c1
+INCBIN "melodies/M02C1.bin"
+.melody_02_c2
+INCBIN "melodies/M02C2.bin"
+.melody_02_c3
+INCBIN "melodies/M02C3.bin"
+
+; Stage
+.melody_03_c3
+INCBIN "melodies/M03C3.bin"
+
+; Stage 2
+.melody_04_c1
+INCBIN "melodies/M04C1.bin"
+.melody_04_c2
+INCBIN "melodies/M04C2.bin"
+.melody_04_c3
+INCBIN "melodies/M04C3.bin"
+
+; God mode
+.melody_05_c1
+INCBIN "melodies/M05C1.bin"
+.melody_05_c2
+INCBIN "melodies/M05C2.bin"
+.melody_05_c3
+INCBIN "melodies/M05C3.bin"
+
+; Bonus
+.melody_06_c1
+INCBIN "melodies/M06C1.bin"
+.melody_06_c2
+INCBIN "melodies/M06C2.bin"
+.melody_06_c3
+INCBIN "melodies/M06C3.bin"
+
+; Fanfare
+.melody_07_c1
+INCBIN "melodies/M07C1.bin"
+.melody_07_c2
+INCBIN "melodies/M07C2.bin"
+.melody_07_c3
+INCBIN "melodies/M07C3.bin"
+
+; Died
+.melody_08_c1
+INCBIN "melodies/M08C1.bin"
+.melody_08_c2
+INCBIN "melodies/M08C2.bin"
+
+; Game over
+.melody_09_c1
+INCBIN "melodies/M09C1.bin"
+.melody_09_c2
+INCBIN "melodies/M09C2.bin"
+.melody_09_c3
+INCBIN "melodies/M09C3.bin"
+
+; Final
+.melody_10_c3
+INCBIN "melodies/M10C3.bin"
